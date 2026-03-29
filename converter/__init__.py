4b1c0168cb7869295d98c2a19ae04614670ddbf4
gilbert12 <gilbert12.tw@gmail.com>
2026-03-29 20:18:05 +0800

    refactor: rename converter/ to pennylane_einsum/, add __version__

--- converter/__init__.py
+++
@@ -1,4 +0,0 @@
-from .circuit_to_einsum import CircuitToEinsum, contract_einsum
-
-__all__ = ["CircuitToEinsum", "contract_einsum"]
-
---
+++ pennylane_einsum/__init__.py
@@ -0,0 +1,5 @@
+from .circuit_to_einsum import CircuitToEinsum, contract_einsum
+from .index_manager import IndexManager
+
+__version__ = "0.1.0"
+__all__ = ["CircuitToEinsum", "contract_einsum", "IndexManager", "__version__"]
